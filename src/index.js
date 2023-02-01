6316039bc2eb10aaee3991a58d3d848cb0a0112a
dTap <imtapped@protonmail.com>
2023-02-01 16:43:03 +0700

    testing nested components

--- src/index.js
+++ src/index.js
@@ -2,25 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 function Greeting() { 
+
   return (
-    // Below is a shorthand version of <React.Fragment></React.Fragment> 
-    // (<></>)
-    <> 
-    <div className='someValue'>
-      <h1>
-        Hello Dickheads!
-      </h1>
-        <ul>
-        <li>
-          <a href="#">Only Dickheads Click Here!</a>
-        </li>
-        </ul>
-      </div>
-      <h2>This is for Asshats</h2>
-      <input type="text" name='someName' id='someID' />
-    </>
+    <div>
+      <h2>John Dickhead</h2>
+      <p>This is my message.</p>
+    </div >
   );
-}
+} 
 
 // function Greeting() { 
 //   return React.createElement('h2', {}, 'Hello Dickhead');
