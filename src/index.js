4efd8c3218ce1afaa527a6fc80a5ea22a8a890b4
dTap <imtapped@protonmail.com>
2023-02-02 21:45:11 +0700

    Refactored code/clean up

--- src/index.js
+++ src/index.js
@@ -2,38 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-const BookList = () => {
-  return <section className='booklist'>
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-  </section>;
+const author = 'Jordan Moore';
+const title = 'Interesting Facts For Curious Minds';
+const img = './images/book-1.jpg';
+
+function BookList() {
+  return (
+    <section className='booklist'>
+      <Book />
+      <Book />
+    </section>
+  );
 }; 
 
-const author = 'Jordan Moore';
 const Book = () => {
-  const title = 'Interesting Facts For Curious Minds';
   return (
     <article className='book'>
-      <img
-        src="./images/book-1.jpg"
-        alt='Interesting Facts For Curious Minds' 
-      />
+      <img src={img} alt={title} /> 
       <h2>{title}</h2>
       <h4>{author}</h4>
     </article>
   );
 };
-
-
-// Below is an example of JSX - CSS (inline styles)
-// const Author = () => (
-//   <h4 style={{ color: 'green', fontSize: '0.75rem', marginTop: '0.5rem' }}>
-//     Jordan Moore
-//   </h4>
-// )
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<BookList />);
