b4342ee56d0ea7b01be3b1f15483dd58487c5312
dTap <imtapped@protonmail.com>
2023-02-03 11:22:59 +0700

    Testing props

--- src/index.js
+++ src/index.js
@@ -6,21 +6,31 @@ const author = 'Jordan Moore';
 const title = 'Interesting Facts For Curious Minds';
 const img = './images/book-1.jpg';
 
+// parameters
+const someFunc = (param1, param2) => {
+  console.log(param1, param2);
+};
+// arguments
+someFunc('job', 'developer');
+
+
+
 function BookList() {
   return (
     <section className='booklist'>
-      <Book />
-      <Book />
+      <Book author={author} title={title} img={img} />
+      <Book author={author} title={title} img={img} />
     </section>
   );
 }; 
 
-const Book = () => {
+const Book = (props) => {
+  console.log(props);
   return (
     <article className='book'>
-      <img src={img} alt={title} /> 
-      <h2>{title}</h2>
-      <h4>{author}</h4>
+      <img src={props.img} alt={props.title} /> 
+      <h2>{props.title}</h2>
+      <h4>{props.author}</h4>
     </article>
   );
 };
