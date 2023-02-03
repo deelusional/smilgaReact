6422d0a851c58881fdb55970a8df031ab199fc96
dTap <imtapped@protonmail.com>
2023-02-03 12:42:17 +0700

    created a list using names.map()

--- src/index.js
+++ src/index.js
@@ -1,38 +1,41 @@
+import { logDOM } from '@testing-library/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+const books = [
+  {
+    author: 'Jordan Moore',
+    title: 'Interesting Facts For Curious Minds',
+    img: './images/book-1.jpg'
+  },
+  {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: './images/book-2.jpg'
+  }
+];
 
-const firstBook = {
-  author: 'Jordan Moore',
-  title: 'Interesting Facts For Curious Minds',
-  img: './images/book-1.jpg'
-}
-
-const secondBook = {
-  author: 'James Clear',
-  title: 'Atomic Habits',
-  img: './images/book-2.jpg'
-}
-
-
+const names = ['john', 'peter', 'susan'];
+const newNames = names.map((name) => {
+  console.log(name);
+  return <h1>{name}</h1>;
+});
 
 function BookList() {
+  return <section className='booklist'>{newNames}</section>;
+}
+
+// Or we can destructure the props if knowing the object is there
+const Book = ({ img, title, author }) => {
   return (
-    <section className='booklist'>
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.img} 
-      />
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        img={secondBook.img} 
-      />
-    </section>
+    <article className='book'>
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+    </article>
   );
-}; 
+};
 
 //  This was the first way we used props. There are many different ways
 // const Book = (props) => {
@@ -58,17 +61,6 @@ function BookList() {
 //     </article>
 //   )
 // }
- 
-// Or we can destructure the props if knowing the object is there
-const Book = ({ img, title, author }) => {
-  return (
-    <article className='book'>
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <h4>{author}</h4>
-    </article>
-  );
-};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<BookList />);
