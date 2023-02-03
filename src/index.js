a3840d736858dc39f2859dafeeee32cc0fe82fcb
dTap <imtapped@protonmail.com>
2023-02-03 12:12:20 +0700

    Cleaner version of writing props

--- src/index.js
+++ src/index.js
@@ -34,16 +34,28 @@ function BookList() {
   );
 }; 
 
+//  This was the first way we used props. 
+// const Book = (props) => {
+//   console.log(props);
+//   return (
+//     <article className='book'>
+//       <img src={props.img} alt={props.title} /> 
+//       <h2>{props.title}</h2>
+//       <h4>{props.author}</h4>
+//     </article>
+//   );
+// };
+// Below is an alternate/cleaner version of using props
 const Book = (props) => {
-  console.log(props);
+  const { img, title, author } = props;
   return (
     <article className='book'>
-      <img src={props.img} alt={props.title} /> 
-      <h2>{props.title}</h2>
-      <h4>{props.author}</h4>
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
     </article>
-  );
-};
+  )
+ }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<BookList />);
