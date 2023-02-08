8a8e8f7d9be5721dd9782e7f0b07dbc431061fa5
dTap <imtapped@protonmail.com>
2023-02-08 15:19:08 +0700

    Test onSubmit/onClick events

--- src/index.js
+++ src/index.js
@@ -32,33 +32,33 @@ function BookList() {
 const EventExamples = () => {
   const handleFormInput = (e) => {
     console.log(e);
-    console.log(e.target);
-    console.log(e.target.name);
-    console.log(e.target.value);
-    console.log('handle form input');
+    // e.target - element
+    console.log(`Input Name : ${e.target.name}`);
+    console.log(`Input Value : ${e.target.value}`);
+    // console.log('handle form input');
   };
   const handleButtonClick = () => {
     alert('handle button click');
   };
   const handleFormSubmission = (e) => {
     e.preventDefault();
-    console.log('Form Submitted');
+    console.log('form submitted');
   };
   return (
     <section>
+      {/* add onSubmit Event Handler */}
       <form onSubmit={handleFormSubmission}>
         <h2>Typical Form</h2>
         <input
           type='text'
-          name='product'
+          name='example'
           onChange={handleFormInput}
           style={{ margin: '1rem 0' }}
         />
-      <button type='submit'>submit</button>
-      <div>
-          <button onClick={handleButtonClick} type='button'>click me</button>
-      </div>
+        {/* add button with type='submit' */}
+        <button type='submit'>submit form</button>
       </form>
+      <button onClick={handleButtonClick}>click me</button>
     </section>
   );
 };
