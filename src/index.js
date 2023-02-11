750e46f78e44eb2e55a37fa587c7aaae484a8198
dTap <imtapped@protonmail.com>
2023-02-11 18:14:26 +0700

    Test displayValue

--- src/index.js
+++ src/index.js
@@ -19,30 +19,27 @@ const books = [
 ];
 
 function BookList() {
+  const someValue = 'shakeAndBake';
+  const displayValue = () => {
+    console.log(someValue);
+  };
   return (
     <section className='booklist'>
-      
       {books.map((book) => {
-        return <Book {...book} key={book.id} />;
+        return <Book {...book} key={book.id} displayValue={displayValue} />;
       })}
     </section>
   );
 }
 
-
-
-
 const Book = (props) => {
-  const { img, title, author } = props;
-  // console.log(props);
-  const displayTitle = () => { 
-   console.log(title);
-  }
+  const { img, title, author, displayValue } = props;
+
   return (
     <article className='book'>
       <img src={img} alt={title} />
       <h2>{title}</h2>
-      <button onClick={displayTitle}>Display Title</button>
+      <button onClick={displayValue}>click me</button>
       <h4>{author} </h4>
     </article>
   );
