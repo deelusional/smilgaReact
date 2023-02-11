5774150365b98f677429a5fd25ce969fe2a47592
dTap <imtapped@protonmail.com>
2023-02-11 15:09:05 +0700

    onClick displayTitle

--- src/index.js
+++ src/index.js
@@ -34,11 +34,15 @@ function BookList() {
 
 const Book = (props) => {
   const { img, title, author } = props;
-  console.log(props);
+  // console.log(props);
+  const displayTitle = () => { 
+   console.log(title);
+  }
   return (
     <article className='book'>
       <img src={img} alt={title} />
       <h2>{title}</h2>
+      <button onClick={displayTitle}>Display Title</button>
       <h4>{author} </h4>
     </article>
   );
