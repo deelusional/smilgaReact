ced4d377cd7b87698d651d942e527cdbe7d10d6e
dTap <imtapped@protonmail.com>
2023-02-21 22:11:16 +0700

    Added books.js

---
+++ src/books.js
@@ -0,0 +1,15 @@
+export const books = [
+  {
+    author: 'Jordan Moore',
+    title: 'Interesting Facts For Curious Minds',
+    img: './images/book-1.jpg',
+    id: 1,
+  },
+  {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: './images/book-2.jpg',
+    id: 2,
+  },
+];
+
--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 
-const books = [
-  {
-    author: 'Jordan Moore',
-    title: 'Interesting Facts For Curious Minds',
-    img: './images/book-1.jpg',
-    id: 1,
-  },
-  {
-    author: 'James Clear',
-    title: 'Atomic Habits',
-    img: './images/book-2.jpg',
-    id: 2,
-  },
-];
+import './index.css';
+import { books } from './books';
+
 
 function BookList() {
   return (
